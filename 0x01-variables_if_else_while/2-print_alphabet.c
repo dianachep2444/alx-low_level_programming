d26092e8441ed60543487816099b4fcb7e14f7e5
dianachep2444 <dianachep2444@gmail.com>
2023-06-16 02:18:50 -0700

    I sometimes suffer from insomnia

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,32 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible different combinations of three digits
+ * main - prints the alphabet in lowercase,
+ * followed by a new line
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int n, m, l;
+	char ch;
 
-	for (n = 48; n < 58; n++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-		for (m = 49; m < 58; m++)
-		{
-			for (l = 50; l < 58; l++)
-			{
-				if (l > m && m > n)
-				{
-					putchar(n);
-					putchar(m);
-					putchar(l);
-					if (n != 55 || m != 56)
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-			}
-		}
+		putchar(ch);
 	}
 	putchar('\n');
 	return (0);
