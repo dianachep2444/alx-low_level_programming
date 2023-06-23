ad5ca5304cba658399b699f43b4e8751c92650f7
dianachep2444 <dianachep2444@gmail.com>
2023-06-23 06:16:32 -0700

    positive

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main -assigns a random number to int n every instance
+ * it executes and prints it
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	if (n > 0)
+                printf("%d is positive\n", n);
+        else if (n == 0)
+                printf("%d is zero\n", n);
+        else if (n < 0)
+                printf("%d is negative\n", n);
+        return (0);
+}
